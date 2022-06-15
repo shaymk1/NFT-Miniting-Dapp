1856706e4e0b82b3122d71764e078609c959def3
Mamosebetsi Kekae <mkekae@gmail.com>
2022-06-15 13:57:50 -0700

    finished the layout of the home, and mint pages,installed web3 packages

--- my-app/dapp.config.js
+++ my-app/dapp.config.js
@@ -1,7 +1,7 @@
 const config = {
-  title: 'BoredApe Minting Dapp',
-  description: 'Minting site for NFTs',
-  contractAddress: '0xc3480D8578A9E0099ADD9fF87da48051Aaf3e139',
+  title: 'BoredApe Dapp',
+  description: 'A showcase dapp which is built for you to create your own',
+  contractAddress: '0x0312e42e4b55823a3C41769DC0B07F382dECc247',
   maxMintAmount: 10,
   presaleMaxMintAmount: 3,
   price: 0.01
--- my-app/pages/mint.js
+++ my-app/pages/mint.js
@@ -1,127 +1,4 @@
-import { useConnectWallet, useSetChain, useWallets } from '@web3-onboard/react'
-import { useEffect, useState } from 'react'
-import { config } from '../dapp.config'
-
-import {
-  getMaxSupply,
-  getTotalMinted,
-  isPausedState,
-  isPreSaleState,
-  isPublicSaleState,
-  presaleMint,
-  publicMint
-} from '../utils/interact'
-import { initOnboard } from '../utils/onboard'
-
 export default function Mint() {
-  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
-  const [{ chains, connectedChain, settingChain }, setChain] = useSetChain()
-  const connectedWallets = useWallets()
-
-  const [maxSupply, setMaxSupply] = useState(0)
-  const [totalMinted, setTotalMinted] = useState(0)
-  const [maxMintAmount, setMaxMintAmount] = useState(0)
-  const [paused, setPaused] = useState(false)
-  const [isPublicSale, setIsPublicSale] = useState(false)
-  const [isPreSale, setIsPreSale] = useState(false)
-
-  const [status, setStatus] = useState(null)
-  const [mintAmount, setMintAmount] = useState(1)
-  const [isMinting, setIsMinting] = useState(false)
-  const [onboard, setOnboard] = useState(null)
-
-  useEffect(() => {
-    setOnboard(initOnboard)
-  }, [])
-
-  useEffect(() => {
-    if (!connectedWallets.length) return
-
-    const connectedWalletsLabelArray = connectedWallets.map(
-      ({ label }) => label
-    )
-    window.localStorage.setItem(
-      'connectedWallets',
-      JSON.stringify(connectedWalletsLabelArray)
-    )
-  }, [connectedWallets])
-
-  useEffect(() => {
-    if (!onboard) return
-
-    const previouslyConnectedWallets = JSON.parse(
-      window.localStorage.getItem('connectedWallets')
-    )
-
-    if (previouslyConnectedWallets?.length) {
-      async function setWalletFromLocalStorage() {
-        await connect({
-          autoSelect: {
-            label: previouslyConnectedWallets[0],
-            disableModals: true
-          }
-        })
-      }
-
-      setWalletFromLocalStorage()
-    }
-  }, [onboard, connect])
-
-  useEffect(() => {
-    const init = async () => {
-      setMaxSupply(await getMaxSupply())
-      setTotalMinted(await getTotalMinted())
-
-      setPaused(await isPausedState())
-      setIsPublicSale(await isPublicSaleState())
-      const isPreSale = await isPreSaleState()
-      setIsPreSale(isPreSale)
-
-      setMaxMintAmount(
-        isPreSale ? config.presaleMaxMintAmount : config.maxMintAmount
-      )
-    }
-
-    init()
-  }, [])
-
-  const incrementMintAmount = () => {
-    if (mintAmount < maxMintAmount) {
-      setMintAmount(mintAmount + 1)
-    }
-  }
-
-  const decrementMintAmount = () => {
-    if (mintAmount > 1) {
-      setMintAmount(mintAmount - 1)
-    }
-  }
-
-  const presaleMintHandler = async () => {
-    setIsMinting(true)
-
-    const { success, status } = await presaleMint(mintAmount)
-
-    setStatus({
-      success,
-      message: status
-    })
-
-    setIsMinting(false)
-  }
-  const publicMintHandler = async () => {
-    setIsMinting(true)
-
-    const { success, status } = await publicMint(mintAmount)
-
-    setStatus({
-      success,
-      message: status
-    })
-
-    setIsMinting(false)
-  }
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-full min-h-screen overflow-hidden bg-brand-background ">
       <div className="relative flex flex-col items-center justify-center w-full h-full">
@@ -129,38 +6,19 @@ export default function Mint() {
           src="/images/blur.jpeg"
           className="absolute inset-auto block object-cover w-full min-h-screen animate-pulse-slow"
         />
-
         <div className="flex flex-col items-center justify-center w-full h-full px-2 md:px-10">
           <div className="relative flex flex-col items-center w-full px-2 py-4 rounded-md z-1 md:max-w-3xl bg-gray-900/90 filter backdrop-blur-sm md:px-10">
-            {wallet && (
-              <button
-                className="absolute right-4 bg-indigo-600 transition duration-200 ease-in-out font-chalk border-2 border-[rgba(0,0,0,1)] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)] active:shadow-none px-4 py-2 rounded-md text-sm text-white tracking-wide uppercase"
-                onClick={() =>
-                  disconnect({
-                    label: wallet.label
-                  })
-                }
-              >
-                Disconnect
-              </button>
-            )}
-            <h1 className="mt-3 text-3xl font-bold text-transparent uppercase font-coiny md:text-4xl bg-gradient-to-br from-brand-green to-brand-blue bg-clip-text">
-              {paused ? 'Paused' : isPreSale ? 'Pre-Sale' : 'Public Sale'}
+            <h1 className="mt-3 text-3xl font-bold uppercase font-coiny md:text-4xl text-brand-purple">
+              Pre-Sale
             </h1>
             <h3 className="text-sm tracking-widest text-pink-200">
-              {wallet?.accounts[0]?.address
-                ? wallet?.accounts[0]?.address.slice(0, 8) +
-                  '...' +
-                  wallet?.accounts[0]?.address.slice(-4)
-                : ''}
+              0xc3480D8578A9E0099ADD9fF87da48051Aaf3e139
             </h3>
-
             <div className="flex flex-col w-full mt-10 md:flex-row md:space-x-14 md:mt-14">
               <div className="relative w-full">
                 <div className="absolute z-10 flex items-center justify-center px-4 py-2 text-base font-semibold text-white bg-black border rounded-md font-coiny top-2 left-2 opacity-80 filter backdrop-blur-lg border-brand-purple">
                   <p>
-                    <span className="text-brand-pink">{totalMinted}</span> /{' '}
-                    {maxSupply}
+                    <span className="text-brand-pink">0</span> / 222
                   </p>
                 </div>
 
@@ -170,12 +28,11 @@ export default function Mint() {
                 />
               </div>
 
+              {/*right side */}
+
               <div className="flex flex-col items-center w-full px-4 mt-16 md:mt-0">
                 <div className="flex items-center justify-between w-full font-coiny">
-                  <button
-                    className="flex items-center justify-center h-10 font-bold bg-gray-300 rounded-md w-14 md:w-16 md:h-12 text-brand-background hover:shadow-lg"
-                    onClick={incrementMintAmount}
-                  >
+                  <button className="flex items-center justify-center h-10 font-bold bg-gray-300 rounded-md w-14 md:w-16 md:h-12 text-brand-background hover:shadow-lg">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6 md:h-8 md:w-8"
@@ -193,13 +50,10 @@ export default function Mint() {
                   </button>
 
                   <p className="flex items-center justify-center flex-1 text-3xl font-bold text-center grow text-brand-pink md:text-4xl">
-                    {mintAmount}
+                    1
                   </p>
 
-                  <button
-                    className="flex items-center justify-center h-10 font-bold bg-gray-300 rounded-md w-14 md:w-16 md:h-12 text-brand-background hover:shadow-lg"
-                    onClick={decrementMintAmount}
-                  >
+                  <button className="flex items-center justify-center h-10 font-bold bg-gray-300 rounded-md w-14 md:w-16 md:h-12 text-brand-background hover:shadow-lg">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6 md:h-8 md:w-8"
@@ -218,76 +72,49 @@ export default function Mint() {
                 </div>
 
                 <p className="mt-3 text-sm tracking-widest text-pink-200">
-                  Max Mint Amount: {maxMintAmount}
+                  Max Mint Amount:5
                 </p>
 
+                {/*total price */}
                 <div className="w-full py-4 mt-16 border-t border-b">
                   <div className="flex items-center justify-between w-full text-xl font-coiny text-brand-yellow">
                     <p>Total</p>
-
-                    <div className="flex items-center space-x-3">
-                      <p>
-                        {Number.parseFloat(config.price * mintAmount).toFixed(
-                          2
-                        )}{' '}
-                        ETH
-                      </p>{' '}
-                      <span className="text-gray-400">+ GAS</span>
-                    </div>
+                  </div>
+                  <div className="flex items-center space-x-3">
+                    <p className="text-brand-blue">
+                      0.01 <span className="text-gray-400">ETH</span>
+                    </p>
+                    <span className="text-brand-green">+ Gas</span>
                   </div>
                 </div>
-
-                {/* Mint Button && Connect Wallet Button */}
-                {wallet ? (
-                  <button
-                    className={` ${
-                      paused || isMinting
-                        ? 'bg-gray-900 cursor-not-allowed'
-                        : 'bg-gradient-to-br from-brand-purple to-brand-pink shadow-lg hover:shadow-pink-400/50'
-                    } font-coiny mt-12 w-full px-6 py-3 rounded-md text-2xl text-white  mx-4 tracking-wide uppercase`}
-                    disabled={paused || isMinting}
-                    onClick={isPreSale ? presaleMintHandler : publicMintHandler}
-                  >
-                    {isMinting ? 'Minting...' : 'Mint'}
-                  </button>
-                ) : (
-                  <button
-                    className="w-full px-6 py-3 mx-4 mt-12 text-2xl tracking-wide text-white uppercase rounded-md shadow-lg font-coiny bg-gradient-to-br from-brand-purple to-brand-pink hover:shadow-pink-400/50"
-                    onClick={() => connect()}
-                  >
-                    Connect Wallet
-                  </button>
-                )}
               </div>
+              {/*mint button */}
+              <button className="w-full px-6 py-3 mx-4 mt-12 text-2xl tracking-wide text-white uppercase rounded-md shadow-lg font-coiny bg-gradient-to-br from-brand-purple to-brand-pink hover:shadow-pink-400/50">
+                Connect Wallet
+              </button>
             </div>
+          </div>
+          {/* Status */}
 
-            {/* Status */}
-            {status && (
-              <div
-                className={`border ${
-                  status.success ? 'border-green-500' : 'border-brand-pink-400 '
-                } rounded-md text-start h-full px-4 py-4 w-full mx-auto mt-8 md:mt-4"`}
-              >
-                <p className="flex flex-col space-y-2 text-white text-sm md:text-base break-words ...">
-                  {status.message}
-                </p>
-              </div>
-            )}
-
-            {/* Contract Address */}
-            <div className="flex flex-col items-center w-full py-2 mt-10 border-t border-gray-800">
-              <h3 className="mt-6 text-2xl uppercase font-coiny text-brand-pink">
-                Contract Address
-              </h3>
-              <a
-                href={`https://rinkeby.etherscan.io/address/${config.contractAddress}#readContract`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="mt-4 text-gray-400"
-              >
-                <span className="break-all ...">{config.contractAddress}</span>
-              </a>
-            </div>
+          <div className=" border border-brand-pink-400  rounded-md text-start h-full px-4 py-4 w-full mx-auto mt-8 md:mt-4">
+            <p className="flex flex-col space-y-2 text-white text-sm md:text-base break-words ...">
+             Something went wrong here!
+            </p>
+          </div>
+          {/*contract address */}
+          <div className="flex flex-col items-center w-full py-2 mt-10 border-t border-gray-800">
+            <h3 className="mt-6 text-2xl uppercase font-coiny text-brand-pink">
+              Contract Address
+            </h3>
+            <a
+              href="https://rinkeby.etherscan.io/address/"
+              className="mt-4 text-gray-400"
+              target="_blank"
+            >
+              <span className="break-all ...">
+                0xc3480D8578A9E0099ADD9fF87da48051Aaf3e139
+              </span>
+            </a>
           </div>
         </div>
       </div>
