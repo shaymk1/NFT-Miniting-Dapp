679cf296b33f33e63ab6e5096251ee7f9ec45c6a
Mamosebetsi Kekae <mkekae@gmail.com>
2022-06-16 19:59:58 -0700

    deleted my private key

--- my-app/pages/mint.js
+++ my-app/pages/mint.js
@@ -1,6 +1,126 @@
+import { useState, useEffect } from 'react'
 import { initOnboard } from '../utils/onboard'
+import { useConnectWallet, useSetChain, useWallets } from '@web3-onboard/react'
+import { config } from '../dapp.config'
+import {
+  getTotalMinted,
+  getMaxSupply,
+  isPausedState,
+  isPublicSaleState,
+  isPreSaleState,
+  presaleMint,
+  publicMint
+} from '../utils/interact'
 
 export default function Mint() {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+  const [{ chains, connectedChain, settingChain }, setChain] = useSetChain()
+  const connectedWallets = useWallets()
+
+  const [maxSupply, setMaxSupply] = useState(0)
+  const [totalMinted, setTotalMinted] = useState(0)
+  const [maxMintAmount, setMaxMintAmount] = useState(0)
+  const [paused, setPaused] = useState(false)
+  const [isPublicSale, setIsPublicSale] = useState(false)
+  const [isPreSale, setIsPreSale] = useState(false)
+
+  const [status, setStatus] = useState(null)
+  const [mintAmount, setMintAmount] = useState(1)
+  const [isMinting, setIsMinting] = useState(false)
+  const [onboard, setOnboard] = useState(null)
+
+  useEffect(() => {
+    setOnboard(initOnboard)
+  }, [])
+
+  useEffect(() => {
+    if (!connectedWallets.length) return
+
+    const connectedWalletsLabelArray = connectedWallets.map(
+      ({ label }) => label
+    )
+    window.localStorage.setItem(
+      'connectedWallets',
+      JSON.stringify(connectedWalletsLabelArray)
+    )
+  }, [connectedWallets])
+
+  useEffect(() => {
+    if (!onboard) return
+
+    const previouslyConnectedWallets = JSON.parse(
+      window.localStorage.getItem('connectedWallets')
+    )
+
+    if (previouslyConnectedWallets?.length) {
+      async function setWalletFromLocalStorage() {
+        await connect({
+          autoSelect: {
+            label: previouslyConnectedWallets[0],
+            disableModals: true
+          }
+        })
+      }
+
+      setWalletFromLocalStorage()
+    }
+  }, [onboard, connect])
+
+  useEffect(() => {
+    const init = async () => {
+      setMaxSupply(await getMaxSupply())
+      setTotalMinted(await getTotalMinted())
+
+      setPaused(await isPausedState())
+      setIsPublicSale(await isPublicSaleState())
+      const isPreSale = await isPreSaleState()
+      setIsPreSale(isPreSale)
+
+      setMaxMintAmount(
+        isPreSale ? config.presaleMaxMintAmount : config.maxMintAmount
+      )
+    }
+
+    init()
+  }, [])
+
+  const incrementMintAmount = () => {
+    if (mintAmount < maxMintAmount) {
+      setMintAmount(mintAmount + 1)
+    }
+  }
+
+  const decrementMintAmount = () => {
+    if (mintAmount > 1) {
+      setMintAmount(mintAmount - 1)
+    }
+  }
+
+  const presaleMintHandler = async () => {
+    setIsMinting(true)
+
+    const { success, status } = await presaleMint(mintAmount)
+
+    setStatus({
+      success,
+      message: status
+    })
+
+    setIsMinting(false)
+  }
+  const publicMintHandler = async () => {
+    setIsMinting(true)
+
+    const { success, status } = await publicMint(mintAmount)
+
+    setStatus({
+      success,
+      message: status
+    })
+
+    setIsMinting(false)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full min-h-screen overflow-hidden bg-brand-background ">
       <div className="relative flex flex-col items-center justify-center w-full h-full">
@@ -91,10 +211,7 @@ export default function Mint() {
                 </div>
               </div>
               {/*mint button */}
-              <button
-                className="w-full px-6 py-3 mx-4 mt-12 text-2xl tracking-wide text-white uppercase rounded-md shadow-lg font-coiny bg-gradient-to-br from-brand-purple to-brand-pink hover:shadow-pink-400/50"
-                
-              >
+              <button className="w-full px-6 py-3 mx-4 mt-12 text-2xl tracking-wide text-white uppercase rounded-md shadow-lg font-coiny bg-gradient-to-br from-brand-purple to-brand-pink hover:shadow-pink-400/50">
                 Connect Wallet
               </button>
             </div>
--- my-app/utils/interact.js
+++ my-app/utils/interact.js
@@ -1,18 +1,18 @@
-// const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
-const { MerkleTree } = require('merkletreejs')
-const keccak256 = require('keccak256')
-// const whitelist = require('../scripts/whitelist.js')
+// /* // const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
+// // const { MerkleTree } = require('merkletreejs')
+// // const keccak256 = require('keccak256')
+// // const whitelist = require('../scripts/whitelist.js')
 
-// const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL)
-import { config } from '../dapp.config'
+// // const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL)
+// import { config } from '../dapp.config'
 
-// const contract = require('../artifacts/contracts/BoredApe.sol/BoredApe.json')
-const nftContract = new web3.eth.Contract(contract.abi, config.contractAddress)
+// // const contract = require('../artifacts/contracts/BoredApe.sol/BoredApe.json')
+// // const nftContract = new web3.eth.Contract(contract.abi, config.contractAddress)
 
-// Calculate merkle root from the whitelist array
-const leafNodes = whitelist.map((addr) => keccak256(addr))
-const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
-const root = merkleTree.getRoot()
+// // Calculate merkle root from the whitelist array
+// // const leafNodes = whitelist.map((addr) => keccak256(addr))
+// // const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
+// // const root = merkleTree.getRoot() */
 
 export const getTotalMinted = async () => {
   const totalMinted = await nftContract.methods.totalSupply().call()
@@ -51,104 +51,105 @@ export const presaleMint = async (mintAmount) => {
       status: 'To be able to mint, you need to connect your wallet'
     }
   }
+}
 
-  const leaf = keccak256(window.ethereum.selectedAddress)
-  const proof = merkleTree.getHexProof(leaf)
+  // const leaf = keccak256(window.ethereum.selectedAddress)
+  // const proof = merkleTree.getHexProof(leaf)
 
   // Verify Merkle Proof
-  const isValid = merkleTree.verify(proof, leaf, root)
+//   const isValid = merkleTree.verify(proof, leaf, root)
 
-  if (!isValid) {
-    return {
-      success: false,
-      status: 'Invalid Merkle Proof - You are not on the whitelist'
-    }
-  }
+//   if (!isValid) {
+//     return {
+//       success: false,
+//       status: 'Invalid Merkle Proof - You are not on the whitelist'
+//     }
+//   }
 
-  const nonce = await web3.eth.getTransactionCount(
-    window.ethereum.selectedAddress,
-    'latest'
-  )
+//   const nonce = await web3.eth.getTransactionCount(
+//     window.ethereum.selectedAddress,
+//     'latest'
+//   )
 
-  // Set up our Ethereum transaction
-  const tx = {
-    to: config.contractAddress,
-    from: window.ethereum.selectedAddress,
-    value: parseInt(
-      web3.utils.toWei(String(config.price * mintAmount), 'ether')
-    ).toString(16), // hex
-    data: nftContract.methods
-      .presaleMint(window.ethereum.selectedAddress, mintAmount, proof)
-      .encodeABI(),
-    nonce: nonce.toString(16)
-  }
+//   // Set up our Ethereum transaction
+//   const tx = {
+//     to: config.contractAddress,
+//     from: window.ethereum.selectedAddress,
+//     value: parseInt(
+//       web3.utils.toWei(String(config.price * mintAmount), 'ether')
+//     ).toString(16), // hex
+//     data: nftContract.methods
+//       .presaleMint(window.ethereum.selectedAddress, mintAmount, proof)
+//       .encodeABI(),
+//     nonce: nonce.toString(16)
+//   }
 
-  try {
-    const txHash = await window.ethereum.request({
-      method: 'eth_sendTransaction',
-      params: [tx]
-    })
+//   try {
+//     const txHash = await window.ethereum.request({
+//       method: 'eth_sendTransaction',
+//       params: [tx]
+//     })
 
-    return {
-      success: true,
-      status: (
-        <a href={`https://rinkeby.etherscan.io/tx/${txHash}`} target="_blank">
-          <p>✅ Check out your transaction on Etherscan:</p>
-          <p>{`https://rinkeby.etherscan.io/tx/${txHash}`}</p>
-        </a>
-      )
-    }
-  } catch (error) {
-    return {
-      success: false,
-      status: '😞 Smth went wrong:' + error.message
-    }
-  }
-}
+//     return {
+//       success: true,
+//       status: (
+//         <a href={`https://rinkeby.etherscan.io/tx/${txHash}`} target="_blank">
+//           <p>✅ Check out your transaction on Etherscan:</p>
+//           <p>{`https://rinkeby.etherscan.io/tx/${txHash}`}</p>
+//         </a>
+//       )
+//     }
+//   } catch (error) {
+//     return {
+//       success: false,
+//       status: '😞 Smth went wrong:' + error.message
+//     }
+//   }
+// }
 
-export const publicMint = async (mintAmount) => {
-  if (!window.ethereum.selectedAddress) {
-    return {
-      success: false,
-      status: 'To be able to mint, you need to connect your wallet'
-    }
-  }
+// export const publicMint = async (mintAmount) => {
+//   if (!window.ethereum.selectedAddress) {
+//     return {
+//       success: false,
+//       status: 'To be able to mint, you need to connect your wallet'
+//     }
+//   }
 
-  const nonce = await web3.eth.getTransactionCount(
-    window.ethereum.selectedAddress,
-    'latest'
-  )
+//   const nonce = await web3.eth.getTransactionCount(
+//     window.ethereum.selectedAddress,
+//     'latest'
+//   )
 
-  // Set up our Ethereum transaction
-  const tx = {
-    to: config.contractAddress,
-    from: window.ethereum.selectedAddress,
-    value: parseInt(
-      web3.utils.toWei(String(config.price * mintAmount), 'ether')
-    ).toString(16), // hex
-    data: nftContract.methods.publicSaleMint(mintAmount).encodeABI(),
-    nonce: nonce.toString(16)
-  }
+//   // Set up our Ethereum transaction
+//   const tx = {
+//     to: config.contractAddress,
+//     from: window.ethereum.selectedAddress,
+//     value: parseInt(
+//       web3.utils.toWei(String(config.price * mintAmount), 'ether')
+//     ).toString(16), // hex
+//     data: nftContract.methods.publicSaleMint(mintAmount).encodeABI(),
+//     nonce: nonce.toString(16)
+//   }
 
-  try {
-    const txHash = await window.ethereum.request({
-      method: 'eth_sendTransaction',
-      params: [tx]
-    })
+//   try {
+//     const txHash = await window.ethereum.request({
+//       method: 'eth_sendTransaction',
+//       params: [tx]
+//     })
 
-    return {
-      success: true,
-      status: (
-        <a href={`https://rinkeby.etherscan.io/tx/${txHash}`} target="_blank">
-          <p>✅ Check out your transaction on Etherscan:</p>
-          <p>{`https://rinkeby.etherscan.io/tx/${txHash}`}</p>
-        </a>
-      )
-    }
-  } catch (error) {
-    return {
-      success: false,
-      status: '😞 Smth went wrong:' + error.message
-    }
-  }
-}
+//     return {
+//       success: true,
+//       status: (
+//         <a href={`https://rinkeby.etherscan.io/tx/${txHash}`} target="_blank">
+//           <p>✅ Check out your transaction on Etherscan:</p>
+//           <p>{`https://rinkeby.etherscan.io/tx/${txHash}`}</p>
+//         </a>
+//       )
+//     }
+//   } catch (error) {
+//     return {
+//       success: false,
+//       status: '😞 Smth went wrong:' + error.message
+//     }
+//   }
+// }
